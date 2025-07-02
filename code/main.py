7965eeb5a4fb0cc399ce2d1237cc183dfe7e2cf1
cyvision13 <cyvision13@outlook.com>
2025-07-02 11:09:03 +0330

    use feature_selection in main file

--- code/main.py
+++ code/main.py
@@ -1,7 +1,9 @@
 from install_dependencies import install_dependencies
 from preprocessing import preprocess_data
+from models.feature_selection import feature_selection as feature_selection_main
 
 install_dependencies()
 
 preprocess_data()
 
+feature_selection_main()

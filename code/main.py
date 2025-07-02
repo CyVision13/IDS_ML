1af39eced5751a5703337f2d35643f7170d71057
cyvision13 <cyvision13@outlook.com>
2025-07-02 11:47:38 +0330

    use it in main file

--- code/main.py
+++ code/main.py
@@ -1,9 +1,12 @@
 from install_dependencies import install_dependencies
 from preprocessing import preprocess_data
 from models.feature_selection import feature_selection as feature_selection_main
+from models.run_fuzzy_topsis import run_fuzzy_topsis_feature_selection
 
-install_dependencies()
+# install_dependencies()
+#
+# preprocess_data()
+#
+# feature_selection_main()
 
-preprocess_data()
-
-feature_selection_main()
+run_fuzzy_topsis_feature_selection()

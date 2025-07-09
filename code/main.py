e476f3e5d4a59fccfb659aae893d140bfdbd89f8
cyvision13 <cyvision13@outlook.com>
2025-07-09 12:04:19 +0330

    use dgwo in main file to optimize

--- code/main.py
+++ code/main.py
@@ -2,11 +2,13 @@ from install_dependencies import install_dependencies
 from preprocessing import preprocess_data
 from models.feature_selection import feature_selection as feature_selection_main
 from models.run_fuzzy_topsis import run_fuzzy_topsis_feature_selection
-
+from models.run_dgwo import run_dgwa_feature_selection
 # install_dependencies()
 #
 # preprocess_data()
 #
 # feature_selection_main()
 
-run_fuzzy_topsis_feature_selection()
+# run_fuzzy_topsis_feature_selection()
+
+run_dgwa_feature_selection()

8d4edbe6de0cc11b3ec2f65a57b92cba9137359c
cyvision13 <cyvision13@outlook.com>
2025-09-03 09:18:05 +0330

    fix accuracy err by importing from sklearn package

--- code/models/run_dgwo.py
+++ code/models/run_dgwo.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 # Setup project root
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
